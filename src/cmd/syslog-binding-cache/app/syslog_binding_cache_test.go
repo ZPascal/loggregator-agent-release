277bf08aafba4f35914da40b5b98544584c208b3
Panagiotis Xynos <panagiotis.xynos@sap.com>
2022-07-08 10:53:44 +0100

    Supporting legacy CCAPI

--- src/cmd/syslog-binding-cache/app/syslog_binding_cache_test.go
+++ src/cmd/syslog-binding-cache/app/syslog_binding_cache_test.go
@@ -149,7 +149,174 @@ var _ = Describe("SyslogBindingCache", func() {
 		Expect(b.Hostname).To(Equal("org.space.app-name"))
 
 		b = findBinding(results, "app-id-2")
-		Expect(b.Drains).To(ConsistOf(binding.Drain{Url: "syslog://drain-c"}, binding.Drain{Url:"syslog://drain-d"}))
+		Expect(b.Drains).To(ConsistOf(binding.Drain{Url: "syslog://drain-c"}, binding.Drain{Url: "syslog://drain-d"}))
+		Expect(b.Hostname).To(Equal("org.space.app-name-2"))
+	})
+
+	It("has an HTTP endpoint that returns aggregate drains", func() {
+		sbc = app.NewSyslogBindingCache(config, metricClient, logger)
+		go sbc.Run()
+		defer sbc.Stop()
+		client := plumbing.NewTLSHTTPClient(
+			bindingCacheTestCerts.Cert("binding-cache"),
+			bindingCacheTestCerts.Key("binding-cache"),
+			bindingCacheTestCerts.CA(),
+			"binding-cache",
+		)
+
+		addr := fmt.Sprintf("https://localhost:%d/aggregate", cachePort)
+
+		var resp *http.Response
+		Eventually(func() error {
+			var err error
+			resp, err = client.Get(addr)
+			return err
+		}).Should(Succeed())
+
+		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+		body, err := io.ReadAll(resp.Body)
+		Expect(err).ToNot(HaveOccurred())
+
+		var result []binding.Binding
+		err = json.Unmarshal(body, &result)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(result).To(HaveLen(1))
+		Expect(result[0].Drains).To(ConsistOf(binding.Drain{Url: "syslog://drain-e"}, binding.Drain{Url: "syslog://drain-f"}))
+	})
+})
+
+var _ = Describe("LegacySyslogBindingCache", func() {
+	var (
+		logger       = log.New(GinkgoWriter, "", log.LstdFlags)
+		metricClient *metricsHelpers.SpyMetricsRegistry
+		config       app.Config
+
+		capi *fakeCC
+		sbc  *app.SyslogBindingCache
+
+		cachePort = 50000
+
+		capiTestCerts         = testhelper.GenerateCerts("capiCA")
+		bindingCacheTestCerts = testhelper.GenerateCerts("bindingCacheCA")
+	)
+
+	BeforeEach(func() {
+		r := legacyResults{
+			"app-id-1": legacyAppBindings{
+				Drains:   []string{"syslog://drain-a", "syslog://drain-b"},
+				Hostname: "org.space.app-name",
+			},
+			"app-id-2": legacyAppBindings{
+				Drains:   []string{"syslog://drain-c", "syslog://drain-d"},
+				Hostname: "org.space.app-name-2",
+			},
+		}
+
+		capi = &fakeCC{
+			legacyResults: r,
+			legacy:        true,
+		}
+		capi.startTLS(capiTestCerts)
+
+		config = app.Config{
+			LegacyAPI:          true,
+			APIURL:             capi.URL,
+			APIPollingInterval: 10 * time.Millisecond,
+			APIBatchSize:       1000,
+			APICAFile:          capiTestCerts.CA(),
+			APICertFile:        capiTestCerts.Cert("capi"),
+			APIKeyFile:         capiTestCerts.Key("capi"),
+			APICommonName:      "capi",
+			CacheCAFile:        bindingCacheTestCerts.CA(),
+			CacheCertFile:      bindingCacheTestCerts.Cert("binding-cache"),
+			CacheKeyFile:       bindingCacheTestCerts.Key("binding-cache"),
+			CacheCommonName:    "binding-cache",
+			CachePort:          cachePort,
+			AggregateDrains:    []string{"syslog://drain-e", "syslog://drain-f"},
+		}
+		metricClient = metricsHelpers.NewMetricsRegistry()
+	})
+
+	AfterEach(func() {
+		capi.CloseClientConnections()
+		capi.Close()
+
+		cachePort++
+	})
+
+	It("debug metrics arn't enabled by default", func() {
+		config.MetricsServer.PprofPort = 1234
+		sbc = app.NewSyslogBindingCache(config, metricClient, logger)
+		go sbc.Run()
+		defer sbc.Stop()
+		Consistently(metricClient.GetDebugMetricsEnabled()).Should(BeFalse())
+		Consistently(func() error {
+			_, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/debug/pprof/", config.MetricsServer.PprofPort))
+			return err
+		}).ShouldNot(BeNil())
+	})
+
+	It("debug metrics can be enabled", func() {
+		config.MetricsServer.DebugMetrics = true
+		config.MetricsServer.PprofPort = 1235
+		sbc = app.NewSyslogBindingCache(config, metricClient, logger)
+		go sbc.Run()
+		defer sbc.Stop()
+		Eventually(metricClient.GetDebugMetricsEnabled).Should(BeTrue())
+		var resp *http.Response
+		Eventually(func() error {
+			var err error
+			resp, err = http.Get(fmt.Sprintf("http://127.0.0.1:%d/debug/pprof/", config.MetricsServer.PprofPort))
+			return err
+		}).Should(BeNil())
+		Expect(resp.StatusCode).To(Equal(200))
+	})
+
+	It("polls CAPI on an interval for results", func() {
+		sbc = app.NewSyslogBindingCache(config, metricClient, logger)
+		go sbc.Run()
+		defer sbc.Stop()
+		Eventually(capi.numRequests).Should(BeNumerically(">=", 2))
+	})
+
+	It("has an HTTP endpoint that returns bindings", func() {
+		sbc = app.NewSyslogBindingCache(config, metricClient, logger)
+		go sbc.Run()
+		defer sbc.Stop()
+		client := plumbing.NewTLSHTTPClient(
+			bindingCacheTestCerts.Cert("binding-cache"),
+			bindingCacheTestCerts.Key("binding-cache"),
+			bindingCacheTestCerts.CA(),
+			"binding-cache",
+		)
+
+		addr := fmt.Sprintf("https://localhost:%d/bindings", cachePort)
+
+		var resp *http.Response
+		Eventually(func() error {
+			var err error
+			resp, err = client.Get(addr)
+			return err
+		}).Should(Succeed())
+
+		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+		body, err := io.ReadAll(resp.Body)
+		Expect(err).ToNot(HaveOccurred())
+
+		var results []binding.Binding
+		err = json.Unmarshal(body, &results)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(results).To(HaveLen(2))
+		b := findBinding(results, "app-id-1")
+		Expect(b.Drains).To(ConsistOf(binding.Drain{Url: "syslog://drain-a"}, binding.Drain{Url: "syslog://drain-b"}))
+		Expect(b.Hostname).To(Equal("org.space.app-name"))
+
+		b = findBinding(results, "app-id-2")
+		Expect(b.Drains).To(ConsistOf(binding.Drain{Url: "syslog://drain-c"}, binding.Drain{Url: "syslog://drain-d"}))
 		Expect(b.Hostname).To(Equal("org.space.app-name-2"))
 	})
 
@@ -194,12 +361,20 @@ type appBindings struct {
 	Hostname string          `json:"hostname"`
 }
 
+type legacyResults map[string]legacyAppBindings
+type legacyAppBindings struct {
+	Drains   []string `json:"drains"`
+	Hostname string   `json:"hostname"`
+}
+
 type fakeCC struct {
 	*httptest.Server
 	count           int
 	called          int64
 	withEmptyResult bool
 	results         results
+	legacy          bool
+	legacyResults   legacyResults
 }
 
 func (f *fakeCC) startTLS(testCerts *testhelper.TestCerts) {
@@ -236,23 +411,45 @@ func (f *fakeCC) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (f *fakeCC) serveWithResults(w http.ResponseWriter, r *http.Request) {
-	resultData, err := json.Marshal(struct {
-		Results results `json:"results"`
-	}{
-		Results: f.results,
-	})
-	if err != nil {
-		w.WriteHeader(500)
-		return
-	}
+	if f.legacy {
+		resultData, err := json.Marshal(struct {
+			Results legacyResults `json:"results"`
+		}{
+			Results: f.legacyResults,
+		})
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
 
-	if f.count > 0 {
-		resultData = []byte(`{"results": {}}`)
-	}
+		if f.count > 0 {
+			resultData = []byte(`{"results": {}}`)
+		}
 
-	w.Write(resultData)
-	if f.withEmptyResult {
-		f.count++
+		w.Write(resultData)
+		if f.withEmptyResult {
+			f.count++
+		}
+
+	} else {
+		resultData, err := json.Marshal(struct {
+			Results results `json:"results"`
+		}{
+			Results: f.results,
+		})
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		if f.count > 0 {
+			resultData = []byte(`{"results": {}}`)
+		}
+
+		w.Write(resultData)
+		if f.withEmptyResult {
+			f.count++
+		}
 	}
 }
 
