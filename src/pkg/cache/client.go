4b1982ce58a24f24afa7642227ee27c1107675bc
Felix Hambrecht <felix.hambrecht@sap.com>
2022-07-25 18:31:55 +0200

    Adding support for syslog+mtls drains in the syslog-agent

--- src/pkg/cache/client.go
+++ src/pkg/cache/client.go
@@ -29,12 +29,35 @@ func (c *CacheClient) Get() ([]binding.Binding, error) {
 	return c.get("bindings")
 }
 
-func (c *CacheClient) GetAggregate() ([]binding.Binding, error) {
-	return c.get("aggregate")
+func (c *CacheClient) GetAggregate() ([]string, error) {
+	return c.getAggregate("aggregate")
 }
 
 func (c *CacheClient) get(path string) ([]binding.Binding, error) {
 	var bindings []binding.Binding
+	resp, err := c.h.Get(fmt.Sprintf("%s/"+path, c.cacheAddr))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		io.Copy(io.Discard, resp.Body)
+		resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected http response from binding cache: %d", resp.StatusCode)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&bindings)
+	if err != nil {
+		return nil, err
+	}
+
+	return bindings, nil
+}
+
+func (c *CacheClient) getAggregate(path string) ([]string, error) {
+	var bindings []string
 	resp, err := c.h.Get(fmt.Sprintf("%s/"+path, c.cacheAddr))
 	if err != nil {
 		return nil, err
